204518dff5ae42e720ae2d86fcf9f95e7bffeade
GRAZIELE ALVES SOARES <graziele.asoares@senacsp.edu.br>
2026-02-24 11:37:54 -0300

    aula24-02

---
+++ 01-constantevariaveis/script.js
@@ -0,0 +1,72 @@
+// comentário em uma linha
+
+/* comentários
+ em 
+ varias 
+ linhas*/
+
+ //exibindo um número na tela
+ console.log(2);
+
+ // exibindo tipo de dado na tela
+ console.log(typeof 2.5);
+
+ // imprimindo uma constante
+ const meunumero = 10;
+ console.log(meunumero);
+
+ //string utilizando aspas simples e aspas duplas
+ console.log('Ola mundo');
+ console.log("Olá planeta terra");
+
+ const nome = "Ana Claudia";
+ console.log(nome);
+
+ //Contagem de String
+console.log(nome.length);
+
+//String de numeros
+const valores = "123";
+console.log(valores);
+console.log(typeof valores);
+
+// Pular uma linha na String
+
+console.log("Pular uma linha para mostrar o resultado \nR$125,00");
+
+// Esoaçamento na String
+
+console.log("Dar espaço \tno texto");
+
+// Boolean verdadeiro ou falso
+
+const noite = true;
+const tarde = false;
+console.log(noite);
+console.log(tarde);
+
+
+const maioridade = 18;
+const maiordeidade = maioridade > 17;
+console.log(maiordeidade);
+
+//Variavel de numero flutuante
+let preco  = 19.99;
+preco = 21.99; //mudança de preço
+console.log(preco);
+
+//undefined (variavel declarada mas não inicializada)
+
+let x;
+console.log(x);
+
+// Null (represena ausencia intencional de valor)
+
+let resposta = null;
+console.log(resposta);
+console.log(typeof resposta);
+
+
+
+
+
---
+++ 02-concatenacaointerpolacao/script.js
@@ -0,0 +1,20 @@
+//Concatenação
+const meutexto = "Olá" + "Mundo";
+console.log(meutexto);
+
+const numeronovo = 15;
+console.log("O numero é " + numeronovo);
+
+const meutexto2 = "Olá Mundo" .concat("mundo novamente");
+console.log(meutexto2);
+
+// Interpolando uma string, um numero e uma expressão
+
+const primeironome = "João";
+const idade = 30;
+console.log(`Olá, ${primeironome}, voce tem ${idade} anos.`);
+
+
+//Comparação
+
+console.log("Olá " + primeironome + ", voce tem " + idade + " anos. ");
